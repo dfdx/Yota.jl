3de2b1b462d47529e221c0b10f684187f6c3ef4d
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2019-08-18 00:13:29 +0300

    Add tracing support for callable objects as functions

--- src/trace/interp.jl
+++ src/trace/interp.jl
@@ -2,33 +2,31 @@ import JuliaInterpreter
 import JuliaInterpreter: enter_call, step_expr!, @lookup, Frame, SSAValue, SlotNumber
 
 
-getexpr(fr::Frame, pc::Int) = fr.framecode.src.code[pc]
-current_expr(fr::Frame) = getexpr(fr, fr.pc)
+getexpr(frame::Frame, pc::Int) = frame.framecode.src.code[pc]
+current_expr(frame::Frame) = getexpr(frame, frame.pc)
 
 
 """
-Split JuliaInterpreter call expression into a tuple of 3 elements:
+Split JuliaInterpreter call expression into a tuple of 2 elements:
 
- * function to be called
- * args to this function
- * vars on the tape corresponding to these args
+ * callable (e.g. function or callable struct) & its arguments
+ * vars on the tape corresponding to these function & args
 
 If arguments include free parameters (not SlotNumber or SSAValue), these are recorded
 to the tape as constants
 """
-function split_int_call!(tape::Tape, fr::Frame, frame_vars::Dict, ex)
+function split_int_call!(tape::Tape, frame::Frame, frame_vars::Dict, ex)
     arr = Meta.isexpr(ex, :(=)) ? ex.args[2].args : ex.args
     # for whatever reason JuliaInterpreter wraps some nodes in the original code into QuoteNode
     arr = [isa(x, QuoteNode) ? x.value : x for x in arr]
-    cf = @lookup(fr, arr[1])
-    cargs = [x isa Symbol ? x : @lookup(fr, x) for x in arr[2:end]]
-    cvars = Vector{Int}(undef, length(cargs))
-    for (i, x) in enumerate(arr[2:end])
+    cfargs = [x isa Symbol ? x : @lookup(frame, x) for x in arr]
+    cvars = Vector{Int}(undef, length(cfargs))
+    for (i, x) in enumerate(arr)
         # if isa(x, JuliaInterpreter.SlotNumber) || isa(x, JuliaInterpreter.SSAValue)
         if haskey(frame_vars, x)
             cvars[i] = frame_vars[x]
         else
-            val = x isa Symbol ? x : @lookup(fr, x)
+            val = x isa Symbol ? x : @lookup(frame, x)
             id = record!(tape, Constant, val)
             cvars[i] = id
             if val != x
@@ -38,81 +36,85 @@ function split_int_call!(tape::Tape, fr::Frame, frame_vars::Dict, ex)
             end
         end
     end
-    return cf, cargs, cvars
+    return cfargs, cvars
 end
 
 
 """
 Given a Frame and current expression, extract LHS location (SlotNumber or SSAValue)
 """
-get_location(fr::Frame, ex) = Meta.isexpr(ex, :(=)) ? ex.args[1] : JuliaInterpreter.SSAValue(fr.pc)
+get_location(frame::Frame, ex) = Meta.isexpr(ex, :(=)) ? ex.args[1] : JuliaInterpreter.SSAValue(frame.pc)
 
 is_int_call_expr(ex) = Meta.isexpr(ex, :call) || (Meta.isexpr(ex, :(=)) && Meta.isexpr(ex.args[2], :call))
 is_int_assign_expr(ex) = Meta.isexpr(ex, :(=)) && (isa(ex.args[2], SlotNumber) || isa(ex.args[2], SSAValue))
 
 is_interesting_expr(ex) = is_int_call_expr(ex) || is_int_assign_expr(ex) || Meta.isexpr(ex, :return)
 
-
-function itrace!(f, tape::Tape, argvars...; primitives)
-    args, vars = zip(argvars...)
-    fr = enter_call(f, args...)
-    frame_vars = Dict{Any, Int}(JuliaInterpreter.SlotNumber(i + 1) => v for (i, v) in enumerate(vars))
-    is_interesting_expr(current_expr(fr)) || step_expr!(fr)  # skip non-call expressions
-    ex = current_expr(fr)
+# f, f_var, args, vars
+# f_arg_vars?
+function itrace!(tape::Tape, fargs, vars; primitives)
+    frame = enter_call(fargs...)
+    frame_vars = Dict{Any, Int}(JuliaInterpreter.SlotNumber(i) => v for (i, v) in enumerate(vars))
+    # f might be a callable struct, so we need to record it and add to frame_vars
+    # f_id = record!(tape, Constant, f)  # should not be constant! should caller record it?
+    # frame_vars[SlotNumber(1)] = f
+    is_interesting_expr(current_expr(frame)) || step_expr!(frame)  # skip non-call expressions
+    ex = current_expr(frame)
     while !Meta.isexpr(ex, :return)
         if is_int_assign_expr(ex)
             lhs, rhs = ex.args
             frame_vars[lhs] = frame_vars[rhs]
-            step_expr!(fr)
+            step_expr!(frame)
         elseif is_int_call_expr(ex)
-            # read as "current function", "current arguments", "current variables"
-            cf, cargs, cvars = split_int_call!(tape, fr, frame_vars, ex)
-            loc = get_location(fr, ex)
+            # read as "current function & arguments", "current variables"
+            cfargs, cvars = split_int_call!(tape, frame, frame_vars, ex)
+            cf = cfargs[1]
+            loc = get_location(frame, ex)
             # there are several special cases such as NamedTuples and constructors
             # we replace these with calls to special helper functions
             if cf isa UnionAll && cf <: NamedTuple
                 # replace cf with namedtuple function, adjust arguments
                 names = collect(cf.body.parameters)[1]
                 cf = namedtuple
-                cargs = [names; cargs]
+                insert!(cfargs, 2, names)
                 names_var_id = record!(tape, Constant, names)
                 cvars = [names_var_id; cvars]
             elseif cf isa DataType
                 # constructor, replace with a call to __new__ which we know how to differentiate
                 T = cf
                 cf = __new__
-                cargs = [T; cargs]
+                insert!(cfargs, 2, T)
                 T_var_id = record!(tape, Constant, T)
                 cvars = [T_var_id; cvars]
             elseif cf == Base.tuple
                 cf = __tuple__
-            elseif cf == Base.getfield
+            elseif cf == Base.getfield || (cf == Base.getindex && isa(tape[cvars[2]].val, NamedTuple))
                 # similar to constuctors, there's a special case for __getfield__ in backprop
                 cf = __getfield__
             end
             # if current function is a primitive of a built-in, write it to the tape
             # otherwise recurse into the current function
             if cf in primitives || isa(cf, Core.Builtin) || isa(cf, Core.IntrinsicFunction)
-                step_expr!(fr)
-                retval = @lookup(fr, loc)
-                ret_id = record!(tape, Call, retval, cf, cvars)
+                step_expr!(frame)
+                retval = @lookup(frame, loc)
+                ret_id = record!(tape, Call, retval, cf, cvars[2:end])
                 frame_vars[loc] = ret_id  # for slots it may overwrite old mapping
             else
                 try
-                    retval, ret_id = itrace!(cf, tape, zip(cargs, cvars)...; primitives=primitives)
-                catch
+                    retval, ret_id = itrace!(tape, cfargs, cvars; primitives=primitives)
+               catch
                     println("Failed to trace through function $cf")
                     rethrow()
                 end
                 frame_vars[loc] = ret_id  # for slots it may overwrite old mapping
-                step_expr!(fr)  # can we avoid this double execution?
+                step_expr!(frame)  # can we avoid this double execution?
             end
         else
-            step_expr!(fr)
+            step_expr!(frame)
         end
-        ex = current_expr(fr)
+        ex = current_expr(frame)
     end
-    retval = @lookup(fr, ex.args[1])
+    retval = @lookup(frame, ex.args[1])
     ret_id = frame_vars[ex.args[1]]
     return retval, ret_id  # return var ID of a result variable
 end
@@ -123,12 +125,15 @@ Trace function f with arguments args using JuliaInterpreter
 """
 function itrace(f, args...; primitives=PRIMITIVES, optimize=true)
     tape = Tape(guess_device(args))
-    argvars = Vector(undef, length(args))
-    for (i, arg) in enumerate(args)
+    # record arguments as input variables
+    fargs = Vector(undef, length(args) + 1)
+    vars = Vector{Int}(undef, length(args) + 1)
+    for (i, arg) in enumerate([f, args...])
         id = record!(tape, Input, arg)
-        argvars[i] = (arg, id)
+        fargs[i] = arg
+        vars[i] = i
     end
-    val, resultid = itrace!(f, tape, argvars...; primitives=primitives)
+    val, resultid = itrace!(tape, fargs, vars; primitives=primitives)
     tape.resultid = resultid
     if optimize
         tape = simplify(tape)
