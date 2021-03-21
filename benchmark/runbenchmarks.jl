d120b3e67f1122d742e100379d5cf200069df550
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2021-03-21 17:59:26 +0300

    Fix benchmark launch script

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -1,1 +1,33 @@
-https://raw.githubusercontent.com/lostella/ProximalOperators.jl/460f0c4e450c627f5698c0cf179222107e6de0b8/benchmark/runbenchmarks.jl
+# This file was adapted from Transducers.jl
+# which is available under an MIT license (see LICENSE).
+using PkgBenchmark
+include("pprinthelper.jl")
+
+
+mkconfig(; kwargs...) =
+    BenchmarkConfig(
+        env = Dict(
+            "JULIA_NUM_THREADS" => "1",
+        );
+        kwargs...
+    )
+
+group_target = benchmarkpkg(
+    dirname(@__DIR__),
+    mkconfig(),
+)
+
+group_baseline = benchmarkpkg(
+    dirname(@__DIR__),
+    mkconfig(id = "baseline"),
+)
+
+judgement = judge(group_target, group_baseline)
+
+displayresult(judgement)
+
+printnewsection("Target result")
+displayresult(group_target)
+
+printnewsection("Baseline result")
+displayresult(group_baseline)
