2815686c294523d19a9cf20b445ee1d041e52a43
Andrei Zhabinski <azbs@toloka.ai>
2022-08-27 01:14:29 +0300

    Fix rrule for iterate. Update TagBot config

--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,6 @@
 # not the most robust function, but works in practise
 is_kwfunc(f) = (name = string(f); endswith(name, "##kw") || endswith(name, "##kw\""))
 is_kwfunc(v::Variable) = is_kwfunc(v._op.val)
+
+# REPL utils - unstable API! don't use in library code!
+Base.:(^)(tape::Tape, i::Integer) = tape[V(i)].val
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Yota
 using Yota: gradtape, gradcheck, update_chainrules_primitives!
 using Yota: trace, compile, play!
 using Yota: make_rrule, YotaRuleConfig
-import ChainRulesCore: Tangent, ZeroTangent, rrule_via_ad
+import ChainRulesCore: Tangent, ZeroTangent, NoTangent, rrule_via_ad
 import ChainRulesTestUtils: test_rrule
 
 # test-only dependencies
